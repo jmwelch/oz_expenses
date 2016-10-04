3d57b2c3a3f1e42241f83c3a7fa2a743a7fa7705
Jared Welch <jarwelch@bellsouth.net>
2016-10-04 20:53:56 +1100

    Sets up validations on payer model

--- app/models/payer.rb
+++ app/models/payer.rb
@@ -1,3 +1,14 @@
 class Payer < ApplicationRecord
+  validates :role, inclusion: { in: proc { Payer::ROLES } }
+
   has_many :payments, inverse_of: :payer
+
+  ROLES = {
+    1 => 'admin',
+    2 => 'viewer'
+  }.freeze
+
+  def get_role
+    ROLES.fetch(role, nil)
+  end
 end
