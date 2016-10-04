fa5a38f1e873feabcf517701f3565d06adf02295
Jared Welch <jarwelch@bellsouth.net>
2016-10-04 21:53:48 +1100

    Includes participants in payment table

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,5 +1,15 @@
 class PaymentController < ApplicationController
   def show
-    @payments = Payment.all
+    @payment_hash = {}
+    payer_ids = Payer.all.collect{|p| p.id}
+    Payment.includes(:participants).all.each do |payment|
+      @payment_hash.store(payment, Hash.new())
+      payer_ids.each do |id|
+        @payment_hash[payment].store(id, 0)
+      end
+      payment.participants.each do |part|
+        @payment_hash[payment].store(part.payer_id, part.amount)
+      end
+    end
   end
 end
